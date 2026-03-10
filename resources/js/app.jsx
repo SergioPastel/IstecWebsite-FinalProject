ad88a671ed3267ca10bc3b1bbd4051d36c8beb0a
Sergio Sant'Anna <serginhocls@gmail.com>
2026-03-10 18:20:50 +0000

    Squashed commit of the following:
    commit 0c7970a71cb0f2e1729696077195ba215f2b28ab
    Author: Sergio Sant'Anna <serginhocls@gmail.com>
    Date:   Tue Mar 10 18:17:19 2026 +0000
    
        Implemented i18next library.
    
        This is an essential library we will be using for our translations in the website. Will need to study how dynamic translation will work but for now it is working.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,14 @@
 import './bootstrap';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
+import './i18n' // Imports our translation configuration file, which allows us to use our translations in our pages
 
-createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
-        return pages[`./Pages/${name}.jsx`];
+createInertiaApp({ // Entry point for our Inertia app
+    resolve: name => { // Dynamically imports our pages based on the name provided by Inertia.
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true }); 
+        return pages[`./Pages/${name}.jsx`]; // Returns the component for the requested page.
     },
-    setup({ el, App, props }) {
+    setup({ el, App, props }) { // Sets up the React application by rendering the App component into the DOM element provided by Inertia.
         createRoot(el).render(<App {...props} />);
     },
 });
