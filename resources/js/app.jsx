31bab41258971f48071def90cb0f51c918dad54f
Sergio Sant'Anna <serginhocls@gmail.com>
2026-03-14 20:34:39 +0000

    Added some comments
    Might rearrage them later.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,10 +5,10 @@ import './i18n' // Imports our translation configuration file, which allows us t
 
 createInertiaApp({ // Entry point for our Inertia app
     resolve: name => { // Dynamically imports our pages based on the name provided by Inertia. Loads all pages between front and back
-        const pages = import.meta.glob('./**/*.jsx', { eager: true }); 
-        return pages[`./${name}.jsx`];
+        const pages = import.meta.glob('./**/*.jsx', { eager: true }); // Vite feature that imports files based on a glob pattern. Eager loading will import all pages on startup rather than importing them when needed
+        return pages[`./${name}.jsx`]; // Name comes from our backend and directs to the correct component
     },
-    setup({ el, App, props }) { // Sets up the React application by rendering the App component into the DOM element provided by Inertia.
+    setup({ el, App, props }) { // Sets up the React application by rendering the App component into the DOM element provided by Inertia (el).
         createRoot(el).render(<App {...props} />);
     },
 });
