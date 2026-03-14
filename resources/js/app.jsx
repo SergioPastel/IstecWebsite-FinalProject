978a3465b48ac91155d7d4a41361041a038937ac
Sergio Sant'Anna <serginhocls@gmail.com>
2026-03-14 20:50:15 +0000

    Squashed commit of the following:
    commit 31bab41258971f48071def90cb0f51c918dad54f
    Author: Sergio Sant'Anna <serginhocls@gmail.com>
    Date:   Sat Mar 14 20:34:39 2026 +0000
    
        Added some comments
    
        Might rearrage them later.
    
    commit 848d58fd418a937acbe17119158f4c5dfdf93466
    Author: Sergio Sant'Anna <serginhocls@gmail.com>
    Date:   Thu Mar 12 18:42:18 2026 +0000
    
        Organized front-end file structure
    
        Made an organized folder structure for our front-end, splitting our JS (views/components) between Frontoffice, Backoffice, and Shared. Have also made example Home and Dashboard pages and their respective routes as an example of inertia routing.
        The placeholder files are just for commit purposes and should be removed when new files are added to the folders.
    
    commit 960399b7293410b3a89788568020d458ebd726ea
    Author: Sergio Sant'Anna <serginhocls@gmail.com>
    Date:   Thu Mar 12 17:30:05 2026 +0000
    
        Squashed commit of the following:
    
        commit f43341a52e84c3e9e6fc01dbfaa253f1f6590ed7
        Author: D4rKing <rui.santos.maia@gmail.com>
        Date:   Wed Mar 11 17:26:43 2026 +0000
    
            Changed ER Diagram to light
    
        commit e56413fc553d4d352f84e61bc1548b24fc2cbb76
        Author: D4rKing <rui.santos.maia@gmail.com>
        Date:   Wed Mar 11 17:24:51 2026 +0000
    
            added ER Diagram
    
        commit 364ac44c9b430b4034dcdbac3eca6565447aa7fe
        Author: Sergio Sant'Anna <serginhocls@gmail.com>
        Date:   Tue Mar 10 18:41:22 2026 +0000
    
            Implemented dynamic translation.
    
            Through the changeLanguage function we can now change the language of a page dynamically in the click of a button. Would be wise to place it on a header later on.
    
        commit ad88a671ed3267ca10bc3b1bbd4051d36c8beb0a
        Author: Sergio Sant'Anna <serginhocls@gmail.com>
        Date:   Tue Mar 10 18:20:50 2026 +0000
    
            Squashed commit of the following:
    
            commit 0c7970a71cb0f2e1729696077195ba215f2b28ab
            Author: Sergio Sant'Anna <serginhocls@gmail.com>
            Date:   Tue Mar 10 18:17:19 2026 +0000
    
                Implemented i18next library.
    
                This is an essential library we will be using for our translations in the website. Will need to study how dynamic translation will work but for now it is working.
    
        commit 5d3729f9985ace3e26888b24c86d619951a0164d
        Author: D4rKing <rui.santos.maia@gmail.com>
        Date:   Tue Mar 3 17:42:23 2026 +0000
    
            Revise links in Requisitos.md
    
            Updated links to functional and non-functional requirements.
    
        commit e71378aa74e70aba96d2462d136d1d05d029f34e
        Author: Sergio Sant'Anna <serginhocls@gmail.com>
        Date:   Tue Mar 3 17:38:10 2026 +0000
    
            Added class diagram
    
            Title.
    
        commit d311ffb05e38653e1df0edbc254422baeaff1b6e
        Author: Sergio Sant'Anna <serginhocls@gmail.com>
        Date:   Tue Mar 3 17:34:55 2026 +0000
    
            Added our use cases to the docs
    
            Title

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,11 +4,11 @@ import { createInertiaApp } from '@inertiajs/react';
 import './i18n' // Imports our translation configuration file, which allows us to use our translations in our pages
 
 createInertiaApp({ // Entry point for our Inertia app
-    resolve: name => { // Dynamically imports our pages based on the name provided by Inertia.
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true }); 
-        return pages[`./Pages/${name}.jsx`]; // Returns the component for the requested page.
+    resolve: name => { // Dynamically imports our pages based on the name provided by Inertia. Loads all pages between front and back
+        const pages = import.meta.glob('./**/*.jsx', { eager: true }); // Vite feature that imports files based on a glob pattern. Eager loading will import all pages on startup rather than importing them when needed
+        return pages[`./${name}.jsx`]; // Name comes from our backend and directs to the correct component
     },
-    setup({ el, App, props }) { // Sets up the React application by rendering the App component into the DOM element provided by Inertia.
+    setup({ el, App, props }) { // Sets up the React application by rendering the App component into the DOM element provided by Inertia (el).
         createRoot(el).render(<App {...props} />);
     },
 });
---
+++ resources/js/front/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { useTranslation } from 'react-i18next'; // Importing the useTranslation hook from react-i18next, which allows us to translate
+
+export default function Home() {
+    {/* This will be used on our partial views (header) to translate content */}
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+    };
+
+    return <>
+        <div>
+            <h1>{t("test")}</h1>
+            <label>front/pages/Home</label><br />
+            <button onClick={() => changeLanguage("en")}>English</button>
+            <button onClick={() => changeLanguage("pt")}>Portuguese</button>
+        </div>
+    </>;
+}
