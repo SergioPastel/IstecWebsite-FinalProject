ce985289c2bd5c5d01906de619c68476e959a12a
Sergio Sant'Anna <serginhocls@gmail.com>
2026-04-24 23:18:42 +0100

    Fixed password recovery

---
+++ app/Http/Responses/PasswordResetResponse.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Contracts\PasswordResetResponse as PasswordResetResponseContract;
+use Laravel\Fortify\Fortify;
+
+class PasswordResetResponse implements PasswordResetResponseContract
+{
+    /**
+     * The response status language key.
+     *
+     * @var string
+     */
+    protected string $status;
+
+    /**
+     * Create a new response instance.
+     *
+     * @param  string  $status
+     * @return void
+     */
+    public function __construct(string $status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        return redirect()
+            ->route('login')
+            ->with('success', trans($this->status));
+    }
+}
---
+++ lang/pt/passwords.php
@@ -0,0 +1,10 @@
+<?php
+
+return [ // Laravel's default password reset language lines. Used automatically by the framework's password broker for password reset emails.
+        // As log-in is admin exclusive, english is not required
+    'password' => 'A palavra-passe deve ter pelo menos oito caracteres e corresponder à confirmação.',
+    'reset' => 'A sua palavra-passe foi alterada com sucesso!',
+    'sent' => 'Enviámos o link de reposição de palavra-passe para o seu email!',
+    'token' => 'Este token de reposição de palavra-passe é inválido.',
+    'user' => 'Não conseguimos encontrar um utilizador com esse endereço de email.',
+];
